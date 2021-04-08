ea1ed5049be81c53231826781f9ae505e2c171e8
Ti1mmy <timothyzheng123@gmail.com>
2021-04-08 16:19:07 -0400

    Faster and better

--- main.py
+++ main.py
@@ -22,9 +22,8 @@ def setup():
 @window.event
 def update(delta_time):
     global pi
-    for i in range(5):
+    for i in range(200):
         points = [random.randrange(0, WIDTH), random.randrange(0, HEIGHT)]
-        print(points)
         if math.sqrt(points[0]**2 + points[1]**2) <= WIDTH:
             circle_points.append(points)
         else:
@@ -36,6 +35,7 @@ def on_draw():
     arcade.start_render()
     arcade.draw_points(outside_points, (0, 0, 0), 2)
     arcade.draw_points(circle_points, (255, 0, 0), 2)
+    arcade.draw_rectangle_filled(0, 0, 300, 40, arcade.color.WHITE)
     arcade.draw_text(f'pi={pi}', 0, 0, arcade.color.BLACK)
 
 
