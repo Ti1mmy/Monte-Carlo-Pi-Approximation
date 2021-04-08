6145f08b86699efeb3e9307e0ce7c7a96e565008
Ti1mmy <timothyzheng123@gmail.com>
2021-04-08 12:03:43 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+# Area = pi * r**2
+# Draw circle with radius 1, and randomly plot points inside 1 quadrant
+# Highlight all dots with distance < 1 from (0,0)
+
+
+import arcadeplus as arcade
+import random
+import math
+
+WIDTH = 800
+HEIGHT = 800
+circle_points = []
+outside_points = [[WIDTH, HEIGHT]]
+window = arcade.open_window(WIDTH, HEIGHT, "Monte Carlo Pi Approximation")
+
+
+def setup():
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.schedule(update, 1/60)
+    arcade.run()
+
+@window.event
+def update(delta_time):
+    global pi
+    for i in range(5):
+        points = [random.randrange(0, WIDTH), random.randrange(0, HEIGHT)]
+        print(points)
+        if math.sqrt(points[0]**2 + points[1]**2) <= WIDTH:
+            circle_points.append(points)
+        else:
+            outside_points.append(points)
+    pi = 4*(len(circle_points)/(len(outside_points) + len(circle_points)))
+
+@window.event
+def on_draw():
+    arcade.start_render()
+    arcade.draw_points(outside_points, (0, 0, 0), 2)
+    arcade.draw_points(circle_points, (255, 0, 0), 2)
+    arcade.draw_text(f'pi={pi}', 0, 0, arcade.color.BLACK)
+
+
+if __name__ == '__main__':
+    setup()
